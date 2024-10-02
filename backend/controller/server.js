88375dab4e00e9aa6696ed9e38e07e9be798adca
JohnClaps <johnclaps3@gmail.com>
2024-10-02 03:35:44 +0200

    small change

--- backend/controller/server.js
+++ backend/controller/server.js
@@ -13,15 +13,3 @@ app.get("/adduser",(req,res) => {
 });
 
 app.listen(4000,()=>console.log("Server on localhost:4000"));
-
-// const express = require('express')
-// const app = express()
-// const port = 3001
-
-// app.get('/', (req, res) => {
-//   res.status(200).send('Hello World!');
-// })
-
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}.`)
-// })
